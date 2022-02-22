122464382230d8bf64b1cfb44acb27ecf9b7c767
u2015s <utkarshsingh2015@gmail.com>
2022-02-22 16:00:27 +0530

    active storage removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,12 +5,11 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+// import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
-ActiveStorage.start()
-
+    // ActiveStorage.start()
